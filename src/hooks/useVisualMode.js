ccf4e9e9f71a6341b253e9543d7452f04fc1001f
Sophie Oliver <sophieoliver@Sophies-Machine.local>
2021-11-01 12:50:57 -0700

    monday morn

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace) {
+
+    setHistory(prev => {
+      if (replace) {
+        prev.pop()
+      }
+      prev.push(newMode)
+      return prev;
+    })
+    setMode(newMode);
+  }
+
+  function back() {
+
+    setHistory(prev => {
+
+      if (prev.length > 1) {
+        prev.pop()
+
+        setMode(() => {        
+          return prev[prev.length - 1]
+        })
+      }
+
+      return prev
+    })
+
+  }
+
+  return { mode, transition, back };
+}
