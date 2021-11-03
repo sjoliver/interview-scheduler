c5279295cad0e5bed4d7fc5acc7a011212d4e3cc
Sophie Oliver <sophieoliver@Sophies-Machine.local>
2021-11-03 13:38:50 -0700

    wednesday

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 import InterviewerListItem from "./InterviewerListItem";
 
 import "components/InterviewerList.scss"
@@ -26,3 +27,7 @@ export default function InterviewerList(props) {
   );
 
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
